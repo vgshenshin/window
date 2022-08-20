54e849559026d93050475afbef5789a0cd4500ac
vgshenshin <vg.shenshin@gmail.com>
2022-08-20 15:15:56 +0300

    add tabs in js

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -9,30 +9,26 @@ const modals = () => {
                 if (e.target) {
                     e.preventDefault();
                 }
-                
+
                 modal.style.display = 'block';
-                document.body.style.overflow = 'hidden';
-                // document.body.classList.add('modal-open');
-    
+                document.body.style.overflow = 'hidden';                
             });
         });
 
         close.addEventListener('click', () => {
             modal.style.display = 'none';
-            document.body.style.overflow = '';
-            // document.body.classList.remove('modal-open');
+            document.body.style.overflow = ''; 
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
                 modal.style.display = 'none';
-                document.body.style.overflow = '';
-                // document.body.classList.remove('modal-open');
+                document.body.style.overflow = ''; 
             }
         });
     }
 
-    function showModalByTime(selector,time) {
+    function showModalByTime(selector, time) {
         setTimeout(function() {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = 'hidden';
@@ -41,7 +37,8 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    showModalByTime('.popup', 3000);
+    // showModalByTime('.popup', 60000);
+
 };
 
 export default modals;
