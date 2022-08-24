b9a3fcf4f4b9f93d1655e579d38249cb426bbe51
vgshenshin <vg.shenshin@gmail.com>
2022-08-24 15:27:30 +0300

    added POST request to server

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// const dist = "C:/OpenServer/domains/window";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
